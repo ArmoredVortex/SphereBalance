e55d5a17633181644f38ee1ee6ca44ba35341da6
Rachit-Pandey <66690593+Rachit-Pandey@users.noreply.github.com>
2020-12-17 18:28:29 +0530

    Initial Commit

---
+++ SphereBalance/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    public Vector3 offset;
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothenedPosition = Vector3.Lerp(transform.position, desiredPosition, speed);
+        transform.position = smoothenedPosition;    
+    }
+}
---
+++ SphereBalance/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    public Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate(){
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float VerticalMovement = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontalMovement,0,VerticalMovement);
+        rigidbody.AddForce(movement*speed);
+    }
+}
+
